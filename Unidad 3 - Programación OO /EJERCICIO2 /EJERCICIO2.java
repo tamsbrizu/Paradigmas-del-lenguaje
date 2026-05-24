423e5749b4616b92d9d084eabdce588dc55db765
Tams <tamarabrizuela882@gmail.com>
2026-05-24 19:26:06 -0300

    Create EJERCICIO2.java

---
+++ Unidad 3 - Programación OO /EJERCICIO2 /EJERCICIO2.java
@@ -0,0 +1,11 @@
+package ejercicio2;
+
+public class EJERCICIO2 {
+
+   
+    public static void main(String[] args) {
+        GestorFrecuente g = new GestorFrecuente();
+        Menu m = new Menu(g);
+        m.mostrarMenu();
+    }   
+}
