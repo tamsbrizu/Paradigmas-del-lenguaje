2f4af9768515d9ec2efabf82396df4f8febd6db7
Tams <tamarabrizuela882@gmail.com>
2026-05-24 19:33:04 -0300

    Delete Unidad 1 - Programacion Logica  directory

--- Unidad 1 - Programacion Logica /ejercicio2.pl
+++
@@ -1,5 +0,0 @@
-progenitor(joaquin, marcos).
-progenitor(joaquin, facundo).
-progenitor(florencia, matias).
-progenitor(martia, andrea).
-hermano(X,Y) :- progenitor(Z,X),progenitor(Z,Y), X /= Y.
--- Unidad 1 - Programacion Logica /ejercicio6.pl
+++
@@ -1,2 +0,0 @@
-modulo(X,Y,R) :- X < Y, R is X.
-modulo(X,Y,R) :- X1  is X-Y, modulo(X1,Y,R).
